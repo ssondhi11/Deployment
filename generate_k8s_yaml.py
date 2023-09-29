b5237a531b52be437dc8b728a77f15764dc9195b
ssondhi11 <146195336+ssondhi11@users.noreply.github.com>
2023-09-29 22:38:14 +0530

    Update generate_k8s_yaml.py

--- generate_k8s_yaml.py
+++ generate_k8s_yaml.py
@@ -9,6 +9,37 @@ template_env = jinja2.Environment(loader=template_loader)
 template = template_env.get_template(template_file)
 
 # Define variables from Jenkins environment
+import sys
+
+# Check if the script received the expected number of arguments
+if len(sys.argv) != 15:
+    print("Usage: generate_k8s_yaml.py --deployment_name <name> --replica_count <count> --app-name <name> --container_name <name> --docker_image <image> --image_tag <tag> --container_port <port> --env_variable_name <name> --env_variable_value <value>")
+    sys.exit(1)
+
+# Parse command-line arguments
+args = sys.argv[1:]
+
+# Define variables based on command-line arguments
+deployment_name_index = args.index("--deployment_name")
+deployment_name = args[deployment_name_index + 1]
+replica_count_index = args.index("--replica_count")
+replica_count = args[replica_count_index + 1]
+app_name_index = args.index("--app-name")
+app_name = args[app_name_index + 1]
+container_name_index = args.index("--container_name")
+container_name = args[container_name_index + 1]
+docker_image_index = args.index("--docker_image")
+docker_image = args[docker_image_index + 1]
+image_tag_index = args.index("--image_tag")
+image_tag = args[image_tag_index + 1]
+container_port_index = args.index("--container_port")
+container_port = args[container_port_index + 1]
+env_variable_name_index = args.index("--env_variable_name")
+env_variable_name = args[env_variable_name_index + 1]
+env_variable_value_index = args.index("--env_variable_value")
+env_variable_value = args[env_variable_value_index + 1]
+
+# Use the variables in your script as needed
 
 
 # Render the template with variables
