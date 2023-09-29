14dda8bee076df6b3f35bd625054fc1d9a337d1b
ssondhi11 <146195336+ssondhi11@users.noreply.github.com>
2023-09-29 23:47:18 +0530

    Update generate_k8s_yaml.py

--- generate_k8s_yaml.py
+++ generate_k8s_yaml.py
@@ -8,7 +8,18 @@ template_loader = jinja2.FileSystemLoader(searchpath="./")
 template_env = jinja2.Environment(loader=template_loader)
 template = template_env.get_template(template_file)
 
-
+# Define variables from Jenkins environment
+deployment_name = "nginx-deployment"
+replica_count = "3"
+app_name = "web"
+container_name = "nginx"
+docker_image = "nginx"
+image_tag = "latest"
+container_port =  "80"
+cpu_limit = "0.5"
+memory_limit = "256Mi"
+env_variable_name = "version"
+env_variable_value = "1.0"
 
 # Render the template with variables
 rendered_template = template.render(
