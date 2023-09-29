a9371871cafc9589dd53825653808bc0bfa0a280
ssondhi11 <146195336+ssondhi11@users.noreply.github.com>
2023-09-29 23:35:07 +0530

    Update generate_k8s_yaml.py

--- generate_k8s_yaml.py
+++ generate_k8s_yaml.py
@@ -8,32 +8,21 @@ template_loader = jinja2.FileSystemLoader(searchpath="./")
 template_env = jinja2.Environment(loader=template_loader)
 template = template_env.get_template(template_file)
 
-# Define variables from Jenkins environment
-deployment_name = "${DEPLOYMENT_NAME}"
-replica_count = "${REPLICA_COUNT}"
-app_name = "${APP_NAME}"
-container_name = "${CONTAINER_NAME}"
-docker_image = "${DOCKER_IMAGE}"
-image_tag = "${IMAGE_TAG}"
-container_port =  "${CONTAINER_PORT}"
-cpu_limit = "${CPU_LIMIT}"
-memory_limit = "${MEMORY_LIMIT}"
-env_variable_name = "${ENV_VARIABLE_NAME}"
-env_variable_value = "${ENV_VARIABLE_VALUE}"
+
 
 # Render the template with variables
 rendered_template = template.render(
-    DEPLOYMENT_NAME=deployment_name,
-    REPLICA_COUNT=replica_count,
-    APP_NAME=app_name,
-    CONTAINER_NAME=container_name,
-    DOCKER_IMAGE=docker_image,
-    IMAGE_TAG=image_tag,
-    CONTAINER_PORT=container_port,
-    CPU_LIMIT=cpu_limit,
-    MEMORY_LIMIT=memory_limit,
-    ENV_VARIABLE_NAME=env_variable_name,
-    ENV_VARIABLE_VALUE=env_variable_value,
+    DEPLOYMENT_NAME,
+    REPLICA_COUNT,
+    APP_NAME,
+    CONTAINER_NAME,
+    DOCKER_IMAGE,
+    IMAGE_TAG,
+    CONTAINER_PORT,
+    CPU_LIMIT,
+    MEMORY_LIMIT,
+    ENV_VARIABLE_NAME,
+    ENV_VARIABLE_VALUE,
 )
 
 # Write the rendered template to deployment.yaml
