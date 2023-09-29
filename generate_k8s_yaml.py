a5fc23601c8167847f8af6577a4f8e41c97eb2ae
ssondhi11 <146195336+ssondhi11@users.noreply.github.com>
2023-09-29 22:40:48 +0530

    Update generate_k8s_yaml.py

--- generate_k8s_yaml.py
+++ generate_k8s_yaml.py
@@ -11,7 +11,7 @@ template = template_env.get_template(template_file)
 # Define variables from Jenkins environment
 import sys
 
-# Check if the script received the expected number of arguments
+# Check if the script received the expected number of arguments (14 including script name)
 if len(sys.argv) != 15:
     print("Usage: generate_k8s_yaml.py --deployment_name <name> --replica_count <count> --app-name <name> --container_name <name> --docker_image <image> --image_tag <tag> --container_port <port> --env_variable_name <name> --env_variable_value <value>")
     sys.exit(1)
@@ -20,24 +20,21 @@ if len(sys.argv) != 15:
 args = sys.argv[1:]
 
 # Define variables based on command-line arguments
-deployment_name_index = args.index("--deployment_name")
-deployment_name = args[deployment_name_index + 1]
-replica_count_index = args.index("--replica_count")
-replica_count = args[replica_count_index + 1]
-app_name_index = args.index("--app-name")
-app_name = args[app_name_index + 1]
-container_name_index = args.index("--container_name")
-container_name = args[container_name_index + 1]
-docker_image_index = args.index("--docker_image")
-docker_image = args[docker_image_index + 1]
-image_tag_index = args.index("--image_tag")
-image_tag = args[image_tag_index + 1]
-container_port_index = args.index("--container_port")
-container_port = args[container_port_index + 1]
-env_variable_name_index = args.index("--env_variable_name")
-env_variable_name = args[env_variable_name_index + 1]
-env_variable_value_index = args.index("--env_variable_value")
-env_variable_value = args[env_variable_value_index + 1]
+deployment_name = args[args.index("--deployment_name") + 1]
+replica_count = args[args.index("--replica_count") + 1]
+app_name = args[args.index("--app-name") + 1]
+container_name = args[args.index("--container_name") + 1]
+docker_image = args[args.index("--docker_image") + 1]
+image_tag = args[args.index("--image_tag") + 1]
+container_port = args[args.index("--container_port") + 1]
+env_variable_name = args[args.index("--env_variable_name") + 1]
+env_variable_value = args[args.index("--env_variable_value") + 1]
+
+# Use the variables in your script as needed
+# ...
+
+# Continue with your script logic
+
 
 # Use the variables in your script as needed
 
