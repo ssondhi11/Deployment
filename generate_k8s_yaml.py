168d436d4dcc922c59646d6519f5f9d39512d570
ssondhi11 <146195336+ssondhi11@users.noreply.github.com>
2023-09-29 20:32:15 +0530

    Update generate_k8s_yaml.py

--- generate_k8s_yaml.py
+++ generate_k8s_yaml.py
@@ -11,6 +11,7 @@ template = template_env.get_template(template_file)
 # Define variables from Jenkins environment
 docker_image = "${DOCKER_IMAGE}"
 env_variable = "${ENV_VARIABLE}"
+env_value = "${ENV_VALUE}"
 cpu_limit = "${CPU_LIMIT}"
 memory_limit = "${MEMORY_LIMIT}"
 replicas = "${REPLICAS}"
@@ -34,7 +35,4 @@ rendered_template = template.render(
 with open('k8s-deployment.yaml', 'w') as deployment_file:
     deployment_file.write(rendered_template)
 
-# Create a Kubernetes Service YAML if needed
-# (You can follow a similar process as above to generate k8s-service.yaml)
 
-# You can also add error handling and validation as needed
