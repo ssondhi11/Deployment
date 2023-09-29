0403c5ea7a950477adae09db3c0418fceb76690b
ssondhi11 <146195336+ssondhi11@users.noreply.github.com>
2023-09-29 23:00:38 +0530

    Update generate_k8s_yaml.py

--- generate_k8s_yaml.py
+++ generate_k8s_yaml.py
@@ -14,16 +14,16 @@ template = template_env.get_template(template_file)
 # Render the template with variables
 rendered_template = template.render(
     DEPLOYMENT_NAME="${deployment_name}",
-    REPLICAS=replica_count,
-    APP_NAME=app_name,
-    CONTAINER_NAME=container_name,
-    DOCKER_IMAGE=docker_image,
-    IMAGE_TAG=image_tag,
-    CONTAINER_PORT=container_port,
-    CPU_LIMIT=cpu_limit,
-    MEMORY_LIMIT=memory_limit,
-    ENV_VARIABLE_NAME=env_variable_name,
-    ENV_VARIABLE_VALUE=env_variable_value,
+    REPLICAS="${replica_count}",
+    APP_NAME="${app_name}",
+    CONTAINER_NAME="$container_name}",
+    DOCKER_IMAGE="${docker_image}",
+    IMAGE_TAG="${image_tag}",
+    CONTAINER_PORT="${container_port}",
+    CPU_LIMIT="${cpu_limit}",
+    MEMORY_LIMIT="${memory_limit}",
+    ENV_VARIABLE_NAME="${env_variable_name}",
+    ENV_VARIABLE_VALUE="${env_variable_value}",
 )
 
 # Write the rendered template to deployment.yaml
